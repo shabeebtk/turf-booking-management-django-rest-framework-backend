5e9e615b937dc1a1a92fc05d4f07ffb8ab002dd0
shabeebtk <shabeebtk768@gmail.com>
2024-01-26 10:27:33 +0530

    database connected to cloud

--- backend/settings.py
+++ backend/settings.py
@@ -105,25 +105,14 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DATABASE_NAME'),
-#         'USER' : os.getenv('DATABASE_USER'),
-#         'PASSWORD' : os.getenv('DATABASE_PASSWORD') ,
-#         'HOST' : 'turf-management.czakuo8smn5d.ap-south-1.rds.amazonaws.com',
-#         'PORT' : '5432'
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DATABASE_NAME'),
         'USER' : os.getenv('DATABASE_USER'),
         'PASSWORD' : os.getenv('DATABASE_PASSWORD') ,
-        'HOST' : 'localhost',
-        'PORT' : '5433'
+        'HOST' : 'playoff.czakuo8smn5d.ap-south-1.rds.amazonaws.com',
+        'PORT' : '5432'
     }
 }
 
