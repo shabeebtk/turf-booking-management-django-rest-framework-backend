21b60913cc05c922cbac5b76193467f17b4697bc
shabeebtk <shabeebtk768@gmail.com>
2024-01-27 13:37:55 +0530

    added new cors

--- backend/settings.py
+++ backend/settings.py
@@ -164,7 +164,7 @@ AUTH_USER_MODEL = 'users.User'
 
 # cors headers 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "http://localhost:5173", "https://playoff-dusky.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
