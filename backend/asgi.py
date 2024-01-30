9ac86b007636324c72a07feb735a6fe5cf56650d
shabeebtk <shabeebtk768@gmail.com>
2024-01-30 14:05:16 +0530

    modified asgi

--- backend/asgi.py
+++ backend/asgi.py
@@ -12,7 +12,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from game.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
